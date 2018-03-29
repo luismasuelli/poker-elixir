9ea210ab807ce6cf241f895e98eff29134db077f
Luis masuelli <luismasuelli@hotmail.com>
2018-03-28 22:32:31 -0500

    initial deck with cards initial mix project (no main application)

---
+++ lib/assets/deck.ex
@@ -0,0 +1,56 @@
+defmodule Assets.Deck do
+  @moduledoc false
+
+  require Assets.Deck.Card
+
+  defstruct [:deck]
+
+  def new(wildcards \\ 2, ranks \\ :all, suits \\ :all, copies \\ 1)
+      when (is_integer(wildcards) and wildcards >= 0) and
+           (is_list(suits) or suits == :all or suits == :empty) and
+           (is_list(ranks) or ranks == :all or suits == :empty) and
+           (is_integer(copies) and copies >= 0) do
+    %__MODULE__{deck: add_copies(wildcards, fix_ranks(ranks), fix_suits(suits), copies)}
+  end
+
+  def shuffle(%__MODULE__{deck: deck}) do
+    %__MODULE__{deck: Enum.shuffle(deck)}
+  end
+
+  defp fix_suits(:all) do
+    Assets.Deck.Card.suits
+  end
+
+  defp fix_suits(:empty) do
+    []
+  end
+
+  defp fix_suits(suits) do
+    suits
+  end
+
+  defp fix_ranks(:all) do
+    Assets.Deck.Card.ranks
+  end
+
+  defp fix_ranks(:empty) do
+    []
+  end
+
+  defp fix_ranks(ranks) do
+    ranks
+  end
+
+  defp add_copies(wildcards, ranks, suits, copies) do
+    Enum.reduce(1..copies, [], fn(_, current) ->
+      adding_cards = Enum.reduce(ranks, current, fn(rank, current) ->
+        Enum.reduce(suits, current, fn(suit, current) ->
+          [Assets.Deck.Card.new(rank, suit) | current]
+        end)
+      end)
+      Enum.reduce(1..wildcards, adding_cards, fn(_, current) ->
+        [Assets.Deck.Card.new(:wildcard) | current]
+      end)
+    end)
+  end
+end
---
+++ lib/assets/deck/card.ex
@@ -0,0 +1,46 @@
+defmodule Assets.Deck.Card do
+  @moduledoc false
+
+  defstruct [:rank, :suit]
+
+  defmacro suits do
+    quote do
+      [:spades, :diamonds, :hearts, :clubs]
+    end
+  end
+
+  defmacro ranks do
+    quote do
+      [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    end
+  end
+
+  defmacro text_rank(rank) do
+    quote do
+      elem({2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"}, unquote(rank) - 2)
+    end
+  end
+
+  defguard is_suit(suit) when suit in suits
+  defguard is_rank(rank) when rank >= 2 and rank <= 14
+
+  def new(rank, suit) when rank >= 2 and rank <= 14 and is_suit(suit) do
+    %__MODULE__{rank: rank, suit: suit}
+  end
+
+  def new(:wildcard) do
+    %__MODULE__{rank: nil, suit: nil}
+  end
+end
+
+defimpl String.Chars, for: Assets.Deck.Card do
+  require Assets.Deck.Card
+
+  def to_string(%Assets.Deck.Card{rank: nil, suit: nil}) do
+    "wildcard"
+  end
+
+  def to_string(%Assets.Deck.Card{rank: rank, suit: suit}) do
+    "#{Assets.Deck.Card.text_rank(rank)} of #{suit}"
+  end
+end
