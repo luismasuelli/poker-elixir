8120fddcdc9b72219930ad7d204dc6fffcaa2d49
Luis masuelli <luismasuelli@hotmail.com>
2018-04-02 23:07:25 -0500

    Permutations and combinations will return 0, instead of failing, if r > n (they return 1 for cases 0,0 and return the appropriate calculations for n,n and n,r when n>r). Also will return n in both cases if r is 1.

--- lib/core/math.ex
+++ lib/core/math.ex
@@ -7,13 +7,17 @@ defmodule Core.Math do
 
   def permutations(n, n), do: factorial(n)
   def permutations(n, 0) when is_integer(n) and n >= 0, do: 1
+  def permutations(n, 1) when is_integer(n) and n >= 0, do: n
   def permutations(n, r) when is_integer(n) and is_integer(r) and n > r and r >= 0 do
     Enum.reduce((n - r + 1)..n, 1, &(&1 * &2))
   end
+  def permutations(n, r) when is_integer(n) and is_integer(r), do: 0
 
   def combinations(n, n) when is_integer(n) and n >= 0, do: 1
   def combinations(n, 0) when is_integer(n) and n >= 0, do: 1
+  def combinations(n, 1) when is_integer(n) and n >= 0, do: n
   def combinations(n, r) when is_integer(n) and is_integer(r) and n > r and r >= 0 do
     permutations(n, r) / factorial(r)
   end
+  def combinations(n, r) when is_integer(n) and is_integer(r), do: 0
 end
